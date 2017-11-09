ed06347d05cdf346446397860c3a20c1da44091c
mithran <mithran77@gmail.com>
2017-11-09 12:20:21 +0530

    Test

--- weddingServices/tests/SeleniumTests.py
+++ weddingServices/tests/SeleniumTests.py
@@ -53,3 +53,16 @@ class SeleniumTests(StaticLiveServerTestCase):
         password_input.send_keys(self.testdata['created_user']['password'])
         self.selenium.find_element_by_xpath('//button').click()		
         self.selenium.find_element_by_xpath('/html/body/div[2]/div/span[2]/a').click()
+
+
+	def test_caterer_select(self):
+		# Login
+        self.selenium.get('%s%s' % (self.testdata['server_address'], '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(self.testdata['created_user']['username'])
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys(self.testdata['created_user']['password'])
+        self.selenium.find_element_by_xpath('//button').click()
+
+		# Navigate to caterer detail
+		
